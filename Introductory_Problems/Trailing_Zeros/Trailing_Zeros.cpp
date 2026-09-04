885977a1a9368597081f4eba9356325bc82d7985
Tanisha Vijayvargiya <168197656+Tanisha1015@users.noreply.github.com>
2026-09-05 00:22:49 +0530

    Add C++ solution for Trailing_Zeros

---
+++ Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem Name: Trailing_Zeros
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-09-04
+ */
+
+#include<bits/stdc++.h> 
+ 
+using namespace std; 
+ 
+int main(){
+    int n; 
+    cin>>n; 
+    cout<<n/5; 
+    return 0; 
+}
