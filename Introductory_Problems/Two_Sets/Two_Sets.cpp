7e7f44b362a49c124ccb9bc7ebc4df2c5df13b13
Tanisha Vijayvargiya <168197656+Tanisha1015@users.noreply.github.com>
2026-09-04 23:41:43 +0530

    Add C++ solution for Two_Sets

---
+++ Introductory_Problems/Two_Sets/Two_Sets.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem Name: Two_Sets
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-09-04
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:34:19: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   34 |     for(int i=0; i<v1.size(); i++) cout<<v1[i]<<" ";
+      |                  ~^~~~~~~~~~
+input/code.cpp:36:19: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   36 |     for(int i=0; i<v2.size(); i++) cout<<v2[i]<<" ";
+      |                  ~^~~~~~~~~~
