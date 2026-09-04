053653082872254a039c510cd2e32baa37e47935
Tanisha Vijayvargiya <168197656+Tanisha1015@users.noreply.github.com>
2026-09-05 00:10:00 +0530

    Add C++ solution for Bit_Strings

---
+++ Introductory_Problems/Bit_Strings/Bit_Strings.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem Name: Bit_Strings
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-09-04
+ */
+
+#include<bits/stdc++.h> 
+ 
+using namespace std; 
+const long long MOD = 10e9 + 7; 
+ 
+int main(){
+    long long num = 1; 
+    int n; 
+    cin>>n; 
+    for(int i=1; i<=n; i++){
+        num = (num*2)%MOD; 
+    } 
+    cout<<num;
+    return 0; 
+}
