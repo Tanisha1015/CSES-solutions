19430a6d509c67c813695cf6ffcf019a1b1cc9fa
Tanisha Vijayvargiya <168197656+Tanisha1015@users.noreply.github.com>
2026-09-05 00:36:02 +0530

    Add C++ solution for Trailing_Zeros

--- Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
+++ Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
@@ -12,6 +12,11 @@ using namespace std;
 int main(){
     int n; 
     cin>>n; 
-    cout<<n/5; 
+    int count = 0; 
+     while (n >= 5) {
+        count += n / 5;
+        n /= 5;
+    }
+    cout<<count<<endl;
     return 0; 
 }
